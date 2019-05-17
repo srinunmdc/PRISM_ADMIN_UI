bb226b4762595323ff9efa02ef4e7bed35146c25
Faiz Ahmad <faizahmadf@gmail.com>
2019-05-17 15:50:34 +0530

    change loaders, add eslint configuration for development

--- app/AdvancedSearch.jsx
+++ app/AdvancedSearch.jsx
@@ -1,71 +1,81 @@
 import React, { Component } from "react";
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from "mobx-react";
 import DropDown from "./DropDown";
 
-import Input from "./Input"
-import  AlertTypeResourceStore from "./store/AlertTypeStore";
+import Input from "./Input";
+import AlertTypeResourceStore from "./store/AlertTypeStore";
 
-
-@inject('alertTypeStore')
+@inject("alertTypeStore")
 @observer
-export default class AdvancedSearch extends Component {
+class AdvancedSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categorey: "",
+      serachText: "",
+      emailChecked: true,
+      smsChecked: true,
+      pushChecked: true
+    };
+  }
 
-	constructor(props){
-		super(props);
-		this.state = {
-			categorey:"",
-			serachText:"",
-			emailChecked:true,
-			smsChecked:true,
-			pushChecked:true
+  searchStringSelected = value => {
+    this.setState({ searchString: value.toString() });
 
-		};
-	}
+    AlertTypeResourceStore.setFilteredAlertTypes(this.state.categorey, value);
+  };
 
-	searchStringSelected=(value)=>{
-		this.setState({searchString: value.toString()})
+  onCategoreySlected = categorey => {
+    AlertTypeResourceStore.setCategories(categorey);
+    this.setState({ categorey: categorey });
+    AlertTypeResourceStore.setFilteredAlertTypes(
+      categorey,
+      this.state.serachText
+    );
+  };
 
-		AlertTypeResourceStore.setFilteredAlertTypes(this.state.categorey, value)
+  onEventTypeSlected = categorey => {
+    //this.setState({categorey: categorey});
+    //AlertTypeResourceStore.setFilteredAlertTypes(categorey, this.state.serachText);
+  };
 
-	}
+  handleChangeChk = e => {
+    console.log(e);
+  };
 
-	onCategoreySlected=(categorey)=>{
-		AlertTypeResourceStore.setCategories(categorey);
-		this.setState({categorey: categorey});
-		AlertTypeResourceStore.setFilteredAlertTypes(categorey, this.state.serachText);
-	}
+  render() {
+    const { alertTypeStore } = this.props;
 
-	onEventTypeSlected=(categorey)=>{
-		//this.setState({categorey: categorey});
-		//AlertTypeResourceStore.setFilteredAlertTypes(categorey, this.state.serachText);
-	}
-
-	handleChangeChk = (e) =>{
-		console.log(e);
-	}
-
-	render(){
-
-		const { alertTypeStore } = this.props;
-
-		return(
-			<div className="row">
-				<div className="col-md-12 col-sm-12 col-xs-12">
-					{/* <div className="col-md-3 col-sm-3 col-xs-12">
+    return (
+      <div className="row">
+        <div className="col-md-12 col-sm-12 col-xs-12">
+          {/* <div className="col-md-3 col-sm-3 col-xs-12">
 						<label className="heading-3 text-light">Event Type<span className="di-icon-help-outline help-icon" aria-hidden="true"></span></label>
 					</div> */}
-					<div className="col-md-3 col-sm-3 col-xs-12">
-						<label className="heading-3 text-light">Category<span className="di-icon-help-outline help-icon" aria-hidden="true"></span></label>
-					</div>
-					<div className="col-md-3 col-sm-3 col-xs-12">
-						<label className="heading-3 text-light">Filter Message<span className="di-icon-help-outline help-icon" aria-hidden="true"></span></label>
-					</div>
-					<div className="col-md-3 col-sm-3 col-xs-12">
-						<label className="heading-3 text-light">Channels </label>
-					</div>
-				</div>
-				<div className="col-md-12 col-sm-12 col-xs-12 align-vertically">
-					{/* <div className="col-md-3 col-sm-3 col-xs-12">
+          <div className="col-md-3 col-sm-3 col-xs-12">
+            <label className="heading-3 text-light">
+              Category
+              <span
+                className="di-icon-help-outline help-icon"
+                aria-hidden="true"
+              />
+            </label>
+          </div>
+          <div className="col-md-3 col-sm-3 col-xs-12">
+            <label className="heading-3 text-light">
+              Filter Message
+              <span
+                className="di-icon-help-outline help-icon"
+                aria-hidden="true"
+              />
+            </label>
+          </div>
+          <div className="col-md-3 col-sm-3 col-xs-12">
+            <label className="heading-3 text-light">Channels </label>
+          </div>
+        </div>
+        <div className="col-md-12 col-sm-12 col-xs-12 align-vertically">
+          {/* <div className="col-md-3 col-sm-3 col-xs-12">
 						<DropDown
 							onSelect={this.onCategoreySlected}
 							default={alertTypeStore.alertCategories.selected}
@@ -73,36 +83,56 @@ export default class AdvancedSearch extends Component {
 							width={100}
 						/>
 					</div> */}
-					<div className="col-md-3 col-sm-3 col-xs-12">
-						<DropDown
-							onSelect={this.onCategoreySlected}
-							default={alertTypeStore.alertCategories.selected}
-							data={alertTypeStore.alertCategories.options}
-							width={100}
-						/>
-					</div>
-					<div className="col-md-3 col-sm-3 col-xs-12">
-						<Input
-							textplaceHolder="Start typing to filter..."
-							onChange={this.searchStringSelected}
-							width={100}
-						/>
-					</div>
-					<div className="col-md-3 col-sm-3 col-xs-12">
-						<span className="di-checkbox margin-15"><input className="sm" type="checkbox" checked={this.state.emailChecked} onChange={this.handleChangeChk}/><span
-							className="lbl sm">Email</span></span>
-						
+          <div className="col-md-3 col-sm-3 col-xs-12">
+            <DropDown
+              onSelect={this.onCategoreySlected}
+              default={alertTypeStore.alertCategories.selected}
+              data={alertTypeStore.alertCategories.options}
+              width={100}
+            />
+          </div>
+          <div className="col-md-3 col-sm-3 col-xs-12">
+            <Input
+              textplaceHolder="Start typing to filter..."
+              onChange={this.searchStringSelected}
+              width={100}
+            />
+          </div>
+          <div className="col-md-3 col-sm-3 col-xs-12">
+            <span className="di-checkbox margin-15">
+              <input
+                className="sm"
+                type="checkbox"
+                checked={this.state.emailChecked}
+                onChange={this.handleChangeChk}
+              />
+              <span className="lbl sm">Email</span>
+            </span>
 
-						<span className="di-checkbox margin-15"><input className="sm" type="checkbox" checked={this.state.smsChecked} onChange={this.handleChangeChk}/><span
-							className="lbl sm">Text</span></span>
+            <span className="di-checkbox margin-15">
+              <input
+                className="sm"
+                type="checkbox"
+                checked={this.state.smsChecked}
+                onChange={this.handleChangeChk}
+              />
+              <span className="lbl sm">Text</span>
+            </span>
 
-						<span className="di-checkbox margin-15"><input className="sm" type="checkbox" checked={this.state.pushChecked} onChange={this.handleChangeChk}/><span
-							className="lbl sm">Push</span></span>
-					</div>
-
-				</div>
-			</div>
-		)
-	}
-
+            <span className="di-checkbox margin-15">
+              <input
+                className="sm"
+                type="checkbox"
+                checked={this.state.pushChecked}
+                onChange={this.handleChangeChk}
+              />
+              <span className="lbl sm">Push</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default AdvancedSearch;
--- app/editor/Editor.jsx
+++ app/editor/Editor.jsx
@@ -1,174 +1,196 @@
-'use strict';
-import React from 'react';
+"use strict";
+import React from "react";
 import CKEditor from "./NewCKEditor";
 import AlertTemplateService from "../service/AlertTemplateService";
+
 export default class Editor extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            edited: false,
-            editMode:false,
-            height: '400px'
-        };
-
-    }
-    onChange = (evt) => {
-        this.setState({
-            edited: true
-        });
-        if(this.props.data.templateContentType == "EMAIL_BODY"){
-            this.props.data.changedContent = evt.editor.getData();
-        } else {
-
-            this.props.data.changedContent = evt.editor.getData().replace('<p>','').replace('</p>', '')//evt.editor.document.getBody().getText();
-           /* // For PUSH, SMS and MAIL_SUBJECT content should be plain and thymeleaf tags are in html, logic to handle the CKEditor formating
+  constructor(props) {
+    super(props);
+    this.state = {
+      edited: false,
+      editMode: false,
+      height: "400px"
+    };
+  }
+  onChange = evt => {
+    this.setState({
+      edited: true
+    });
+    if (this.props.data.templateContentType == "EMAIL_BODY") {
+      this.props.data.changedContent = evt.editor.getData();
+    } else {
+      this.props.data.changedContent = evt.editor
+        .getData()
+        .replace("<p>", "")
+        .replace("</p>", ""); //evt.editor.document.getBody().getText();
+      /* // For PUSH, SMS and MAIL_SUBJECT content should be plain and thymeleaf tags are in html, logic to handle the CKEditor formating
             var dynamicParams = newContent.match(/\$\{([^}]+)\}/gmi);
             var i;
             for (i=0;i<dynamicParams.length;i++) {
                 //  var field = dynamicParams[i].substring(2, dynamicParams[i].length-1)
                 newContent = newContent.replace(dynamicParams[i], '<span th:text="'+dynamicParams[i]+'">'+dynamicParams[i]+'<span>');
             }*/
-        }
-        //this.props.data.changed = true;
-       // AlertTemplateResourceStore.updateTemplateResource(this.props.data)
-
     }
+    //this.props.data.changed = true;
+    // AlertTemplateResourceStore.updateTemplateResource(this.props.data)
+  };
 
-    render() {
+  render() {
+    const previewDivStyle = {
+      height: this.state.height,
+      border: "1px solid #d1d1d1",
+      overflow: "scroll"
+    };
 
-        const previewDivStyle = {
-            height: this.state.height,
-            border: '1px solid #d1d1d1',
-            overflow:'scroll'
-        };
-
-        return (
-            <div>
-                <div className="col-md-12 col-sm-12 col-xs-12" >
-                    {this.state.editMode ?
-                        <div className="col-md-10 col-sm-10 col-xs-12">
-                            <CKEditor
-                                activeClass="p10"
-                                content={this.props.data.templateContent}
-                                events={{
-                                    "change": this.onChange
-
-                                }}
-                                config={{
-                                    language: this.props.data.locale,
-                                    height: this.state.height,
-                                    toolbarCanCollapse: true,
-                                    allowedContent: true,
-                                    disableAutoInline: true,
-                                    forcePasteAsPlainText: true,
-                                    removeButtons: 'PasteText,PasteFromWord,Indent,Outdent,Scayt,Link,Unlink,Anchor,Image,Table,HorizontalRule,SpecialChar,Maximize,Strike,RemoveFormat,NumberedList,BulletedList,Blockquote,Styles,About,Subscript,Superscript'
-
-                                }}
-
-                            />
-                        </div>
-                        :
-                        <div className="col-md-10 col-sm-10 col-xs-12" style={previewDivStyle}>
-                            <div dangerouslySetInnerHTML={{__html:this.props.data.changedContent}}></div>
-                        </div>
-                    }
-                </div>
-
-                <div className="col-md-12 col-sm-12 col-xs-12">
-                        {this.props.data.state && this.props.data.state === 'DRAFT' && this.state.edited === false ?
-                            <div>
-                                <div className="col-xs-2 pull-right">
-                                    <button type="button" className="btn sm btn-primary"
-                                            onClick={this.onPublish}>Publish
-                                    </button>
-                                </div>
-                                <div className="col-xs-2 pull-right">
-                                    <button type="button" className="btn sm btn-primary"
-                                            onClick={this.onReject}>Reject
-                                    </button>
-                                </div>
-                            </div>
-                            : null
-                        }
-                        {this.state.edited ?
-                            <div>
-                                <div className="col-xs-2 pull-right">
-                                    <button type="button" className="btn sm btn-primary"
-                                            onClick={this.onDraft}>Save as Draft
-                                    </button>
-                                </div>
-                                <div className="col-xs-2 pull-right">
-                                    <button type="button" className="btn sm btn-primary"
-                                        onClick={this.onCancel}>Cancel
-                                    </button>
-                                </div>
-                            </div>
-                            :null
-                        }
-                        {this.state.editMode ?
-                            <div className="col-xs-2 pull-right">
-                                <button type="button" className="btn sm btn-primary"
-                                        onClick={this.onPreview}>Preview
-                                </button>
-                            </div>
-                            :
-                            <div className="col-xs-2 pull-right">
-                                <button type="button" className="btn sm btn-primary"
-                                        onClick={this.onClickEdit}>Edit
-                                </button>
-                            </div>
-                        }
-
-                </div>
-
-
-
+    return (
+      <div>
+        <div className="col-md-12 col-sm-12 col-xs-12">
+          {this.state.editMode ? (
+            <div className="col-md-10 col-sm-10 col-xs-12">
+              <CKEditor
+                activeClass="p10"
+                content={this.props.data.templateContent}
+                events={{
+                  change: this.onChange
+                }}
+                config={{
+                  language: this.props.data.locale,
+                  height: this.state.height,
+                  toolbarCanCollapse: true,
+                  allowedContent: true,
+                  disableAutoInline: true,
+                  forcePasteAsPlainText: true,
+                  removeButtons:
+                    "PasteText,PasteFromWord,Indent,Outdent,Scayt,Link,Unlink,Anchor,Image,Table,HorizontalRule,SpecialChar,Maximize,Strike,RemoveFormat,NumberedList,BulletedList,Blockquote,Styles,About,Subscript,Superscript"
+                }}
+              />
             </div>
+          ) : (
+            <div
+              className="col-md-10 col-sm-10 col-xs-12"
+              style={previewDivStyle}
+            >
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: this.props.data.changedContent
+                }}
+              />
+            </div>
+          )}
+        </div>
 
-        );
-    }
+        <div className="col-md-12 col-sm-12 col-xs-12">
+          {this.props.data.state &&
+          this.props.data.state === "DRAFT" &&
+          this.state.edited === false ? (
+            <div>
+              <div className="col-xs-2 pull-right">
+                <button
+                  type="button"
+                  className="btn sm btn-primary"
+                  onClick={this.onPublish}
+                >
+                  Publish
+                </button>
+              </div>
+              <div className="col-xs-2 pull-right">
+                <button
+                  type="button"
+                  className="btn sm btn-primary"
+                  onClick={this.onReject}
+                >
+                  Reject
+                </button>
+              </div>
+            </div>
+          ) : null}
+          {this.state.edited ? (
+            <div>
+              <div className="col-xs-2 pull-right">
+                <button
+                  type="button"
+                  className="btn sm btn-primary"
+                  onClick={this.onDraft}
+                >
+                  Save as Draft
+                </button>
+              </div>
+              <div className="col-xs-2 pull-right">
+                <button
+                  type="button"
+                  className="btn sm btn-primary"
+                  onClick={this.onCancel}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          ) : null}
+          {this.state.editMode ? (
+            <div className="col-xs-2 pull-right">
+              <button
+                type="button"
+                className="btn sm btn-primary"
+                onClick={this.onPreview}
+              >
+                Preview
+              </button>
+            </div>
+          ) : (
+            <div className="col-xs-2 pull-right">
+              <button
+                type="button"
+                className="btn sm btn-primary"
+                onClick={this.onClickEdit}
+              >
+                Edit
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 
-    onDraft = () => {
-        console.log("Saving Templates")
-        this.props.data.templateContent = this.props.data.changedContent;
-        AlertTemplateService.saveTemplate(this.props.data);
-        this.setState({
-            edited: false
-        })
+  onDraft = () => {
+    console.log("Saving Templates");
+    this.props.data.templateContent = this.props.data.changedContent;
+    AlertTemplateService.saveTemplate(this.props.data);
+    this.setState({
+      edited: false
+    });
+  };
 
-    }
+  onClickEdit = () => {
+    this.setState({
+      editMode: true
+    });
+  };
 
-    onClickEdit = () => {
-        this.setState({
-            editMode : true
-        })
-    }
+  onPreview = () => {
+    this.setState({
+      editMode: false
+    });
+  };
 
-    onPreview = () => {
-        this.setState({
-            editMode : false
-        })
-    }
+  onPublish = () => {
+    console.log("Saving Templates");
+    AlertTemplateService.publishTemplate(this.props.data);
+  };
 
-    onPublish = () => {
-        console.log("Saving Templates")
-        AlertTemplateService.publishTemplate(this.props.data);
-    }
+  onCancel = () => {
+    this.setState({
+      edited: false,
+      editMode: false
+    });
+    this.props.data.changedContent = this.props.data.templateContent;
+  };
 
-    onCancel = () => {
-        this.setState({
-            edited: false,
-            editMode : false
-        })
-        this.props.data.changedContent = this.props.data.templateContent;
-
-    }
-
-    onReject = () => {
-        console.log("Deleting Template")
-        this.setState({
-            editMode : false
-        })
-        AlertTemplateService.deleteTemplate(this.props.data);
-    }
+  onReject = () => {
+    console.log("Deleting Template");
+    this.setState({
+      editMode: false
+    });
+    AlertTemplateService.deleteTemplate(this.props.data);
+  };
 }
