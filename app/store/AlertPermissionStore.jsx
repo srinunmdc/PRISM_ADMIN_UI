cdbdfdeaae76adaae29524bcd2b6002898f2c9f9
kapa <srinu.nmdc@gmail.com>
2019-05-22 14:13:46 +0530

    Add files via upload

---
+++ app/store/AlertPermissionStore.jsx
@@ -0,0 +1,25 @@
+'use strict'
+import { observable, action, configure, toJS } from 'mobx';
+configure({ enforceActions: 'observed' });
+
+class AlertPermissionStore {
+    @observable permissions = {};
+
+    @action
+    setPermissions = (permissions) => {
+        if (permissions) {
+            this.permissions = permissions
+
+        }
+
+    }
+
+
+    resetStore = () => {
+        this.permissions = {};
+
+    };
+
+}
+const AlertPermissionResourceStore = new AlertPermissionStore();
+export default AlertPermissionResourceStore;
